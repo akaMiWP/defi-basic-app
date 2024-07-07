4eb3faa6259f3ef86d5c80cb4d1400bfe2cab2bf
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-08 00:52:54 +0700

    Added a feature to connect with web3 account

--- src/Components/Account.tsx
+++ src/Components/Account.tsx
@@ -1,9 +1,21 @@
 import { Box, Button, HStack, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import truncateMiddle from "../Helpers/String+Ext";
+import { ethers } from "ethers";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 const Account = () => {
-  const [account, setAccount] = useState<string | null>("123");
+  const [account, setAccount] = useState<string | null>(null);
+
+  async function requestWeb3Account() {
+    const accounts: string[] = await provider.send("eth_requestAccounts", []);
+    setAccount(accounts[0]);
+  }
+
+  useEffect(() => {
+    requestWeb3Account();
+  }, []);
 
   return (
     <>
@@ -14,7 +26,10 @@ const Account = () => {
               {truncateMiddle(account, 12)}
             </Text>
           )}
-          <Button color={account ? "orange.200" : "teal.50"}>
+          <Button
+            color={account ? "orange.200" : "teal.50"}
+            onClick={requestWeb3Account}
+          >
             {account ? "Connected" : "Connect"}
           </Button>
         </HStack>
