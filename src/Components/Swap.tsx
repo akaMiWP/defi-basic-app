795abf26df53af73aac70e7f5394db23aef5b369
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-08 21:05:26 +0700

    Working on Swap component

--- src/Components/Swap.tsx
+++ src/Components/Swap.tsx
@@ -2,21 +2,45 @@ import {
   Box,
   Button,
   Center,
-  Container,
-  HStack,
+  Flex,
+  Icon,
+  Input,
   Spacer,
+  Text,
 } from "@chakra-ui/react";
-import React from "react";
+
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 const Swap = () => {
   return (
-    <Center marginTop={16}>
-      <HStack width="400px" color="orange.50" marginBottom={4}>
-        <Button>Swap</Button>
-        <Spacer></Spacer>
-      </HStack>
-      <Container></Container>
-    </Center>
+    <>
+      <Center marginTop={16}>
+        <Box w="30%" justifyContent="center">
+          <Button>Swap</Button>
+        </Box>
+      </Center>
+      <Center marginTop={4}>
+        <Box
+          w="30%"
+          bg="teal.900"
+          justifyContent="center"
+          height="125px"
+          borderRadius={12}
+        >
+          <Text fontSize="sm" padding={4}>
+            Sell
+          </Text>
+          <Flex paddingLeft={4} paddingRight={4}>
+            <Input placeholder="0" variant="unstyled" fontSize="2xl" />
+            <Spacer />
+            <Button borderRadius={18}>
+              <Text>ETH</Text>
+              <ChevronDownIcon marginLeft={2} />
+            </Button>
+          </Flex>
+        </Box>
+      </Center>
+    </>
   );
 };
 
