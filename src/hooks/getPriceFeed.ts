cf312b992599cd668189804000231b6758316596
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-14 14:32:53 +0700

    Added new ABI methods + Implemented apporve and swapTokens from client side

--- src/hooks/getPriceFeed.ts
+++ src/hooks/getPriceFeed.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import tokens from "../data/tokens";
 
 // const provider = new ethers.providers.AlchemyProvider(
 //   "sepolia",
@@ -6,28 +7,101 @@ import { ethers } from "ethers";
 // );
 
 const provider = new ethers.providers.Web3Provider(window.ethereum, "sepolia");
-
 const address = import.meta.env.VITE_CONSUMER_V3_ADDRESS;
-const abi = [
+
+const dexABI = [
   {
-    inputs: [],
+    inputs: [
+      {
+        components: [
+          {
+            internalType: "string",
+            name: "ticker",
+            type: "string",
+          },
+          {
+            internalType: "address",
+            name: "aggregatorAddress",
+            type: "address",
+          },
+        ],
+        internalType: "struct DefiConsumerV3.PriceFeedPair[]",
+        name: "priceFeedPairs",
+        type: "tuple[]",
+      },
+    ],
     stateMutability: "nonpayable",
     type: "constructor",
   },
   {
+    anonymous: false,
     inputs: [
       {
-        internalType: "string",
-        name: "priceFeed",
-        type: "string",
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
       },
     ],
-    name: "getChainlinkDataFeedLatestAnswer",
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "token",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "depositToken",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "token1",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "token2",
+        type: "address",
+      },
+    ],
+    name: "getPriceFeedFromPair",
     outputs: [
       {
         internalType: "int256",
         name: "",
         type: "int256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
       },
     ],
     stateMutability: "view",
@@ -52,19 +126,380 @@ const abi = [
     stateMutability: "view",
     type: "function",
   },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "token1",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "token2",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "swapTokens",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
-const contract = new ethers.Contract(address, abi, provider);
+const erc20ABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "symbol",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "totalSupply",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "subtractedValue",
+        type: "uint256",
+      },
+    ],
+    name: "decreaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "addedValue",
+        type: "uint256",
+      },
+    ],
+    name: "increaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
-export default async function getPriceFeed(sell: string, buy: string) {
+const dexContract = new ethers.Contract(address, dexABI, provider);
+
+export async function getPriceFeed(
+  sellTokenTicker: string,
+  buyTokenTicker: string
+) {
   try {
-    const priceFeed: string = sell + "/" + buy;
-    const priceInBigInt = await contract.getChainlinkDataFeedLatestAnswer(
-      priceFeed
+    const priceFeedInBigInt: number = await dexContract.getPriceFeedFromPair(
+      tokens[sellTokenTicker],
+      tokens[buyTokenTicker]
     );
-    const price = ethers.utils.formatEther(priceInBigInt);
-    console.log(priceFeed, price);
+
+    const price = ethers.utils.formatEther(priceFeedInBigInt);
+    console.log(sellTokenTicker, "/", buyTokenTicker, price, priceFeedInBigInt);
     return price;
   } catch (error) {
     console.log(error);
   }
 }
+
+export const approve = async (tokenAddress: string, input: string) => {
+  const tokenContract = new ethers.Contract(tokenAddress, erc20ABI, provider);
+  const amount = ethers.utils.parseEther(input);
+  const txResponse = await tokenContract.approve(address, amount);
+  console.log("Approve Trasanction Response:", txResponse);
+};
+
+export const swapTokens = async (
+  token1: string,
+  token2: string,
+  input: string
+) => {
+  const txResponse = await dexContract.swapTokens(
+    tokens[token1],
+    tokens[token2],
+    ethers.utils.parseEther(input)
+  );
+  console.log("Swap Transaction Response:", txResponse);
+};
