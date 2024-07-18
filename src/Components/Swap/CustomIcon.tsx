6b0d66d1652ae3b68ba2cce698ed7a4b7805581d
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-18 22:00:12 +0700

    Removed unused codes

--- src/Components/Swap/CustomIcon.tsx
+++ src/Components/Swap/CustomIcon.tsx
@@ -7,16 +7,6 @@ interface Props {
 }
 
 const CustomIcon = ({ path, boxSize }: Props) => {
-  //   console.log("Path:", path);
-
-  //   const icon = createIcon({
-  //     displayName: "CustomIcon",
-  //     viewBox: "0 0 30 30",
-  //     path: <image href={path} x="0" y="0" width="30" height="30" />,
-  //   });
-
-  //   return <Icon as={icon} boxSize={boxSize} />;
-
   return (
     <Icon viewBox="0 0 40 40" boxSize={boxSize}>
       <image href={path} x="0" y="0" width="40" height="40" />
